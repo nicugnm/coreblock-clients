b3d9bc22f3ca52a3e151f168a42393eab04b2b08
nicugnm <nicolae.ghergu@s.unibuc.ro>
2022-04-26 01:37:41 +0300

    Implement (somehow) reactive rest - R2DBC & CrudReactiveRepository + Database postgresql with flyway.

---
+++ src/test/java/ro/coreblock/clients/CoreblockClientsApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.coreblock.clients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreblockClientsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
